2e1113ea385353c124f03f752fe5a83c16830a82
Ferdiawan <ferdiawanrandy@gmail.com>
2022-01-29 18:36:34 +0700

    First commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0xff5C40CC);
+Color kWhiteColor = Color(0xffffffff);
+Color kGreyColor = Color(0xffC0C1CA);
+Color kGreenColor = Color(0xff0EC3AE);
+Color kRedColor = Color(0xffEB70A5);
+Color kBackgroundColor = Color(0xff353941);
+Color kInnerBackgroundColor = Color(0xff26282B);
+Color kInactiveColor = Color(0xffDBD7EC);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
